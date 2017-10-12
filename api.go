b9dfd15718876b37271d2d042edf4cffe1b65620
mecaota <mecaota@users.noreply.github.com>
2017-10-12 22:05:22 +0900

    return json, but unexpected '/'

--- api.go
+++ api.go
@@ -1,16 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"html"
 	"log"
 	"net/http"
 )
 
+// "Body"
+type Body struct {
+	URL  string `json:"url"`
+	Code int    `json:"code"`
+}
+
 func main() {
 	fmt.Printf("Hello world!")
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello, %q", html.EscapeString(r.URL.Path))
+		response, err := json.Marshal(Body{URL: r.URL.Path, Code: 404})
+		if err != nil {
+			fmt.Println("JSON Marshal error:", err)
+			return
+		}
+		fmt.Fprintf(w, "hello, %q", response)
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
